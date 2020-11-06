a49fa5540cabb1c7b31b5dc87d5624872b95760f
RulLu16 <may05200@naver.com>
2020-11-06 18:05:21 +0900

    week 7. PCA

---
+++ Week 7/lab7/PCA/stdafx.cpp
@@ -0,0 +1,11 @@
+
+// stdafx.cpp : source file that includes just the standard includes
+// PCA.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+#ifdef _DEBUG
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#endif
+
